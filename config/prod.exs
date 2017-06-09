fb0349171819f22406cf2d3fa91a1059a2035899
Troels Irgens-Møller <troelsirgensmoeller@gmail.com>
2017-06-09 10:34:26 +0200

    Fix missing database config for prod

--- config/prod.exs
+++ config/prod.exs
@@ -25,7 +25,7 @@ config :headsup, Headsup.Web.Endpoint,
 config :logger, level: :info
 
 # Configure your database
-config :hello_phoenix, HelloPhoenix.Repo,
+config :headsup, Headsup.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
