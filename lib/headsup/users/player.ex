78213e89f30dc9dfbacf677cccb9810d59f8ba13
Troels Irgens-Møller <troelsirgensmoeller@gmail.com>
2017-06-10 19:47:23 +0200

    Slugs for players

--- lib/headsup/users/player.ex
+++ lib/headsup/users/player.ex
@@ -6,15 +6,17 @@ defmodule Headsup.Users.Player do
 
   schema "users_players" do
     field :name, :string
+    field :slug, :string
     many_to_many :subscriptions, Headsup.Users.Subscription, join_through: Headsup.Users.PlayerSubscription, on_replace: :delete
     timestamps()
   end
 
   @doc false
   def changeset(%Player{} = player, attrs) do
+    attrs = attrs |> Map.put(:slug, Slugger.slugify_downcase(attrs.name))
     player
-    |> cast(attrs, [:name])
-    |> validate_required([:name])
+    |> cast(attrs, [:name, :slug])
+    |> validate_required([:name, :slug])
     |> unique_constraint(:name)
   end
 end
